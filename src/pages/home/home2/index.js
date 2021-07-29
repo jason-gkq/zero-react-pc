fd7d4cbd1c7220bc9d6c78ebbeecbf39d7422cb8
jason-gkq <guokeqin163@163.com>
2021-07-29 19:39:31 +0800

    测试弹窗

--- src/pages/home/home2/index.js
+++ src/pages/home/home2/index.js
@@ -5,53 +5,69 @@ import { BasePage } from "@/common/core";
 import model from "./index.model";
 
 import { globalActions } from "@/common/redux";
-
+import { Modal } from "antd";
 @BasePage(model)
 class Home extends Component {
   constructor(props) {
     super(props);
     // console.log("home2-ownProps", props);
-  }
-  // static model = model;
-  static getConfig() {
-    return {
-      pageId: "10011",
-      name: "home2",
-      barSettings: {
-        title: { text: "修改名片" },
-        leftItems: [{ type: 1 }],
-        rightItems: [
-          {
-            text: "保存",
-            onPress: "$saveMessage",
-          },
-        ],
-      },
+    this.state = {
+      isModalVisible: false,
     };
+    this.handleOk = this.handleOk.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+    this.showModal = this.showModal.bind(this);
   }
-
+  handleOk() {
+    this.setState({
+      isModalVisible: false,
+    });
+  }
+  handleCancel() {
+    this.setState({
+      isModalVisible: false,
+    });
+  }
+  showModal() {
+    this.setState({
+      isModalVisible: true,
+    });
+  }
   render() {
-    const { dispatch } = this.props;
+    const { dispatch, addVoucher, goTo } = this.props;
+    const { isModalVisible } = this.state;
     return (
-      <div>
-        {/* <PageContent/> */}
-        <div className={styles.testContainer}>
-          <div onClick={this.props.addVoucher} className={styles.containerDiv}>
-            我是一个home2
-          </div>
-          <div onClick={this.props.goTo} className={styles.containerDiv}>
-            我是asdfasdfdiv{" "}
-          </div>
-          <div
-            onClick={() => {
-              dispatch(globalActions.navigate.goBack());
-            }}
-            className={styles.containerDiv}
-          >
-            我是一个更多字而且第三个div
+      <>
+        <Modal
+          title='Basic Modal'
+          visible={isModalVisible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+        <div>
+          {/* <PageContent/> */}
+          <div className={styles.testContainer}>
+            <div onClick={this.showModal} className={styles.containerDiv}>
+              我是一个home2
+            </div>
+            <div onClick={goTo} className={styles.containerDiv}>
+              我是asdfasdfdiv{" "}
+            </div>
+            <div
+              onClick={() => {
+                dispatch(globalActions.navigate.goBack());
+              }}
+              className={styles.containerDiv}
+            >
+              我是一个更多字而且第三个div
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
@@ -66,7 +82,6 @@ export default connect(
     return {
       addVoucher() {
         // store.dispatch(store.globalActions.test());
-        dispatch(model.actions.addVer("dsfds"));
       },
       goTo() {
         dispatch(

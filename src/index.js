a5c24f5f21cda08e8b1c9c26e7cb91aedd3ef24a
jason-gkq <guokeqin163@163.com>
2021-08-05 21:59:32 +0800

    性能分析

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-/**
- * 项目入口
- *  web
- *  小程序入口
- */
 // import 'babel-polyfill';
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
+import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+reportWebVitals((data) => {
+  sessionStorage.setItem(data.name, JSON.stringify(data));
+  if (__ENV__ === "local") {
+    console.log(data);
+  }
+});
+
 serviceWorker.unregister();
 // if (process.env.NODE_ENV === "production") {
 //   const reportWebVitals = require("./reportWebVitals").default;

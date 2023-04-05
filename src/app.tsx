86cfa82e54cbbd405396953e67f73f9a13bbdd18
guokeqin <guokeqin163@163.com>
2023-04-05 13:15:43 +0800

    加css

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import { createApp } from "@/zero";
 import model from "./app.model";
+import "@/zero/dist/index.css";
 import "./app.less";
 
 export default createApp({ isNeedLogin: true }, model);
